0d07d43347ea1f461e6a05b773267e9053388fbf
caoxb <caoxb@ichaotu.com>
2015-12-30 13:46:08 +0800

    init

---
+++ src/com/roger/screenlocker/MainPopupWindowActivity.java
@@ -0,0 +1,13 @@
+package com.roger.screenlocker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPopupWindowActivity  extends Activity {
+	@Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+		super.onCreate(savedInstanceState);  
+	    setContentView(R.layout.activity_mainpopupwindow);  
+	}
+
+}
